086a8fe9acee44971533be29d72185ef3e1542f0
Alejandro XD <java.util.fck@hotmail.com>
2018-10-21 18:32:16 -0500

    Installed node. Modified to run with Budo

--- 1_firstGame/src/main.js
+++ 1_firstGame/src/main.js
@@ -4,4 +4,25 @@ const gameTypeString = "Telegram Bot";
 alert (`Let's make a ${gameTypeString} game!`);
 */
 
-const myGuess = Math.floor(Math.random * 20) + 1;
+const myGuess = Math.floor(Math.random() * 20 /*+ 1*/);
+let guesses = 0;
+let guess;
+let done = false;
+while (!done){
+    guess = parseInt(prompt("What number am I thinking of?! " + myGuess), 10);
+    guesses++;
+    
+    switch (true){
+        case (guess < myGuess):
+            alert("Go Higher!");
+            break;
+        case (guess > myGuess):
+            alert("Go Lower!");
+            break;
+        default:
+            done = true;
+            alert(`Well done! Finished in ${guesses}  guesses`);
+            guesses = 0;
+            break;
+    }
+}
